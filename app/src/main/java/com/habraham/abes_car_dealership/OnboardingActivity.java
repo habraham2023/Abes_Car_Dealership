60dcaffbcffeba0671067907c1d0ce40de4c46b3
Abraham Hernandez <habraham@fb.com>
2020-07-13 06:44:36 -0400

    Added Bare Onboarding, Login and Signup Activities

---
+++ app/src/main/java/com/habraham/abes_car_dealership/OnboardingActivity.java
@@ -0,0 +1,53 @@
+package com.habraham.abes_car_dealership;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.parse.ParseUser;
+
+public class OnboardingActivity extends AppCompatActivity {
+    private static final String TAG = "OnboardingActivity";
+
+    MaterialButton btnLogin;
+    MaterialButton btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        // Direct user to Main screen if they are already logged in
+        if (ParseUser.getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        // Direct user to Login screen when login button is pressed.
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: Login");
+                Intent intent = new Intent(OnboardingActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Direct user to Signup screen when signup button is pressed.
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: Signup");
+                Intent intent = new Intent(OnboardingActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/habraham/abes_car_dealership/SignupActivity.java
+++ app/src/main/java/com/habraham/abes_car_dealership/SignupActivity.java
@@ -2,16 +2,13 @@ package com.habraham.abes_car_dealership;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.parse.ParseUser;
-
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
     }
 }
