3d104c5848a810e553b6a5c15a4a40fc146c51a1
Abraham Hernandez <habraham@fb.com>
2020-07-13 08:00:36 -0400

    Wired up Login Activity

--- app/src/main/java/com/habraham/abes_car_dealership/LoginActivity.java
+++ app/src/main/java/com/habraham/abes_car_dealership/LoginActivity.java
@@ -2,13 +2,71 @@ package com.habraham.abes_car_dealership;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    MaterialButton btnSignup;
+    MaterialButton btnContinue;
+    TextInputLayout usernameTextInput;
+    TextInputEditText usernameEditText;
+    TextInputLayout passwordTextInput;
+    TextInputEditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignup = findViewById(R.id.btnSignup);
+        btnContinue = findViewById(R.id.btnContine);
+
+        usernameTextInput = findViewById(R.id.username_text_input);
+        usernameEditText = findViewById(R.id.username_edit_text);
+
+        passwordTextInput = findViewById(R.id.password_text_input);
+        passwordEditText = findViewById(R.id.password_edit_text);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                attemptSignIn(username, password);
+            }
+        });
+    }
+
+    private void attemptSignIn(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    passwordTextInput.setError("Username or password is incorrect.");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
     }
 }
