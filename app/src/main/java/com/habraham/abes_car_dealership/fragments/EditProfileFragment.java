fae140473caaa67976bd3c0300f31ea5e28a7d0b
Abraham Hernandez <habraham@fb.com>
2020-07-15 10:45:10 -0400

    Added new profile picture functionality

--- app/src/main/java/com/habraham/abes_car_dealership/fragments/EditProfileFragment.java
+++ app/src/main/java/com/habraham/abes_car_dealership/fragments/EditProfileFragment.java
@@ -1,16 +1,24 @@
 package com.habraham.abes_car_dealership.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
@@ -18,20 +26,26 @@ import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.habraham.abes_car_dealership.R;
-import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
+
 public class EditProfileFragment extends Fragment {
+    private static final String TAG = "EditProfileFragment";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    public String photoFileName = "profilePicture.jpg";
+    protected File photoFile;
     ImageView ivProfileImage;
     Toolbar toolbar;
-
     TextInputLayout screenNameInputLayout;
     TextInputEditText screenNameEditText;
     TextInputLayout bioInputLayout;
     TextInputEditText bioEditText;
-
     MaterialButton btnTakeNew;
     MaterialButton btnSave;
 
@@ -82,7 +96,7 @@ public class EditProfileFragment extends Fragment {
         btnTakeNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                launchCamera();
+                launchCamera();
             }
         });
 
@@ -105,6 +119,7 @@ public class EditProfileFragment extends Fragment {
                 ParseUser user = ParseUser.getCurrentUser();
                 user.put("screenName", newScreenName);
                 user.put("bio", newBio);
+                user.put("profilePicture", new ParseFile(photoFile));
 
                 user.saveInBackground(new SaveCallback() {
                     @Override
@@ -117,5 +132,49 @@ public class EditProfileFragment extends Fragment {
         });
     }
 
+    public void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
 
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider.Abes_Community_Car_Dealership", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // Start the image capture intent to take photo
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    private File getPhotoFileUri(String photoFileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + photoFileName);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                Glide.with(getContext()).load(takenImage).transform(new CircleCrop()).into(ivProfileImage);
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
