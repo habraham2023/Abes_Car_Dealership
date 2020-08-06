4192b0c3ec4feaf718b3fd0b7673b1efcc91a74c
Abraham Hernandez <habraham@fb.com>
2020-08-06 11:27:30 -0400

    Can view photos with no crop and zoom

---
+++ app/src/main/java/com/habraham/abes_car_dealership/fragments/ImageFragmentDialog.java
@@ -0,0 +1,92 @@
+package com.habraham.abes_car_dealership.fragments;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.habraham.abes_car_dealership.R;
+import com.habraham.abes_car_dealership.databinding.FragmentImageBinding;
+
+public class ImageFragmentDialog extends DialogFragment {
+    private static final String TAG = "ImageFragmentDialog";
+    private static final String ARG_IMAGE = "image";
+    private FragmentImageBinding binding;
+    private String imageURL;
+    ImageView ivSlide;
+
+    public ImageFragmentDialog() {
+        // Required empty public constructor
+    }
+
+
+    public static ImageFragmentDialog newInstance(String imageURL) {
+        ImageFragmentDialog fragment = new ImageFragmentDialog();
+        Bundle args = new Bundle();
+        args.putString(ARG_IMAGE, imageURL);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            imageURL = getArguments().getString(ARG_IMAGE);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        // Store access variables for window and blank point
+        Window window = getDialog().getWindow();
+        Point size = new Point();
+        // Store dimensions of the screen in `size`
+        Display display = window.getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        // Set the width of the dialog proportional to 75% of the screen width
+        window.setLayout((int) (size.x), WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        // Call super onResume after sizing
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentImageBinding.inflate(getLayoutInflater(), container, false);
+        View view = binding.getRoot();
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ivSlide = binding.ivSlide;
+        Log.i(TAG, "onViewCreated: " + imageURL);
+        Glide.with(getContext()).load(imageURL).into(ivSlide);
+    }
+}
